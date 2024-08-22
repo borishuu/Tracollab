d5c03f6a56d62b85365e8326ea0fb1e091a0b57a
yahia1018 <yanis.ouadahi@gmail.com>
2024-08-22 16:06:17 +0200

    Some other CRUD examples

--- tracollab/prismainsert.js
+++ tracollab/prismainsert.js
@@ -6,36 +6,54 @@ const prisma = new PrismaClient();
 async function main() {
   try {
     // Insertion d'un nouvel utilisateur dans la base de données
-    const user = await prisma.user.create({
-      data: {
-        email: 'john.doe@example.com',
-        name: 'John Doe',
-        password: 'password',
-        profilePicture: 'https://example.com/profile.jpg',
-        joinDate: new Date()
-      },
-    });
+    // const user = await prisma.user.create({
+    //   data: {
+    //     email: 'john.doe@example.com',
+    //     name: 'John Doe',
+    //     password: 'password',
+    //     profilePicture: 'https://example.com/profile.jpg',
+    //     joinDate: new Date()
+    //   },
+    // });
 
-    // Insertion d'un nouveau genre dans la base de données
-    const genre = await prisma.genre.create({
-      data: {
-        name: 'Hip-Hop',
-      },
-    });
+    // // Insertion d'un nouveau genre dans la base de données
+    // const genre = await prisma.genre.create({
+    //   data: {
+    //     name: 'Hip-Hop',
+    //   },
+    // });
+    //
+    // // Insertion d'un nouveau son dans la base de données
+    // const sound = await prisma.sound.create({
+    //   data: {
+    //     title: 'Sound 1',
+    //     audioPath: 'lien_cloud_storage',
+    //     picture: 'https://example.com/sound1.jpg',
+    //     genreId: genre.id,
+    //   },
+    // });
 
-    // Insertion d'un nouveau son dans la base de données
-    const sound = await prisma.sound.create({
-      data: {
-        title: 'Sound 1',
-        audioPath: 'lien_cloud_storage',
-        picture: 'https://example.com/sound1.jpg',
-        genreId: genre.id, // Trouver comment récupérer l'id du genre Hip-Hop
-        description: 'Description 1',
-      },
-    });
+    // Supprimer des genres
+    // await prisma.genre.findFirst().then(async (genre) => {
+    //   await prisma.genre.delete({
+    //     where: {
+    //       id: genre.id,
+    //     },
+    //   });
+    // });
+
+    // Récupérer un son
+    const retrievedSound = await prisma.sound.findFirst({
+        where: {
+            title: "Sound 1"
+        },
+    }).then((sound) =>
+        // Afficher l'URL du son
+        console.log("URL: " + sound.audioPath)
+    );
 
     // Afficher l'utilisateur créé
-    console.log('Nouvel utilisateur inséré : ', user);
+    // console.log('Nouvel utilisateur inséré : ', user);
   } catch (error) {
     console.error('Erreur lors de l\'écriture dans la base de données :', error);
   } finally {
