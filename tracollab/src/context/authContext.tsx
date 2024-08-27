766468e202b58fd8ec6f7e2c2a65a3caeac7d0d2
borishuu <boris.hutzli@heig-vd.ch>
2024-08-27 14:49:43 +0200

    finished authentication

---
+++ tracollab/src/context/authContext.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import React, { useEffect, useState, createContext, useContext } from "react";
+
+interface AuthContextType {
+    user: Object;
+    setUser: React.Dispatch<React.SetStateAction<string | null>>;
+    fetchData: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+}
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState<Object>(null);
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('/api/user');
+            const data = await response.json();
+            console.log(data);
+            if (data && data.name) {
+                setUser(data.name);
+            } else {
+                setUser(null);
+            }
+        } catch (error) {
+            console.log(error);
+            setUser(null);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, fetchData }}>
+            {!loading ? (children) : (
+                <div className="w-full min-h-screen pt-[10vh] bg-gray-900 flex justify-center items-center">
+                    <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+                </div>
+            )}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthProvider;
--- tracollab/src/middleware.ts
+++ tracollab/src/middleware.ts
@@ -4,25 +4,29 @@ const secret = new TextEncoder().encode(process.env.SECRET_KEY);
 
 export async function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname;
-    const isPublicPath = path === '/' || path === '/register';
+    const isPublicPath = path === '/login' || path === '/register';
+    const hiddenToConnectedUsers = path === '/login' || path === '/register';
     const token = req.cookies.get('authToken')?.value;
 
     if (!token && !isPublicPath) {
-        return NextResponse.redirect(new URL('/', req.url));
+        return NextResponse.redirect(new URL('/login', req.url));
     } else if (token) {
+        if (hiddenToConnectedUsers) {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
         try {
             const { payload } = await jwtVerify(token, secret);
             const tokenExpiration = new Date(payload.exp * 1000);
 
             if (tokenExpiration <= new Date()) {
-                return NextResponse.redirect(new URL('/', req.url));
+                return NextResponse.redirect(new URL('/login', req.url));
             }
         
             if (isPublicPath) {
                 return NextResponse.next();
             }
         } catch (error) {
-            return NextResponse.redirect(new URL('/', req.url));
+            return NextResponse.redirect(new URL('/login', req.url));
         }
     }
     return NextResponse.next();
@@ -30,6 +34,6 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
     matcher: [
-        '/', '/register'
+        '/', '/login', '/register'
     ]
 }
