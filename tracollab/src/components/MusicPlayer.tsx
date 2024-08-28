cfa87fcaf07e35baaf579ef740eb31322f10891f
yahia1018 <yanis.ouadahi@gmail.com>
2024-08-28 16:21:37 +0200

    Readapted according to the schema

--- tracollab/src/components/MusicPlayer.tsx
+++ tracollab/src/components/MusicPlayer.tsx
@@ -6,17 +6,17 @@ export default function MusicPlayer({ post }) {
             <div className="flex">
                 <div className="w-3/4 h-20 z-10 pl-5 pt-3">
                     <p className="text-sm whitespace-nowrap overflow-hidden text-ellipsis max-w-sm">
-                        <span className="inline-block animate-scroll">{post.postableSound?.sound?.title}</span>
+                        <span className="inline-block animate-scroll">{post.sound.title}</span>
                     </p>
                     <p className="text-sm mt-4">by {post.user?.name}</p>
                 </div>
                 <div className="w-1/4 h-20 z-10 pt-3 flex justify-center">
-                    <p className="text-sm">{post.postableSound?.sound?.genre?.name}</p>
+                    <p className="text-sm">{post.sound.genre.name}</p>
                 </div>
             </div>
             <div className="w-full pl-6 pb-1 flex justify-center">
                 <audio controls className="w-full max-w-xs">
-                    <source src={post.postableSound?.sound?.audioPath} type="audio/mpeg" />
+                    <source src={post.sound.audioPath} type="audio/mpeg" />
                     Your browser does not support the audio element.
                 </audio>
             </div>
--- tracollab/src/components/MusicPlayerWithImage.tsx
+++ tracollab/src/components/MusicPlayerWithImage.tsx
@@ -1,18 +1,19 @@
 import MusicPlayer from "@/components/MusicPlayer";
 import React from 'react';
 
-export default function MusicPlayerWithImage({ post }) {
-    const hasSoundData = post.postableSound?.sound;
-    const hasPicture = post.postableSound?.sound?.picture;
+export default function MusicPlayerWithImage({post}) {
+    const hasSoundData = post?.sound;
+    const hasPicture = hasSoundData?.picture
 
     return (
         <div className="flex-1 p-4 flex items-center">
             <div className="w-32 flex items-center justify-center rounded-lg">
                 {hasSoundData && hasPicture ? (
-                    <div className="w-32 h-32 bg-[#8ACE00] flex items-center justify-center text-white rounded-full overflow-hidden">
+                    <div
+                        className="w-32 h-32 bg-[#8ACE00] flex items-center justify-center text-white rounded-full overflow-hidden text-center">
                         <img
-                            src={post.postableSound.sound.picture}
-                            alt={post.postableSound.sound.title}
+                            src={post.sound.picture}
+                            alt={post.description}
                             width={96}
                             height={96}
                             className="object-cover w-full h-full"
@@ -25,7 +26,7 @@ export default function MusicPlayerWithImage({ post }) {
                 )}
             </div>
             <div className="flex-1 pl-4">
-                <MusicPlayer post={post} />
+                <MusicPlayer post={post}/>
             </div>
         </div>
     );
