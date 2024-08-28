766da5107ea608f25723734a3be7af0685750e2d
yahia1018 <yanis.ouadahi@gmail.com>
2024-08-28 13:49:41 +0200

    Good advancement with players

--- tracollab/src/components/MusicPlayer.tsx
+++ tracollab/src/components/MusicPlayer.tsx
@@ -1,26 +1,25 @@
-
 import React from 'react';
 
-export default function MusicPlayer() {
+export default function MusicPlayer({ post }) {
     return (
-        <div className="relative text-white rounded-3xl overflow-hidden bg-[#9732C2]">
+        <div className="relative text-white rounded-3xl overflow-hidden bg-[#9732C2] max-w-sm">
             <div className="flex">
-                <div className="w-3/4  h-20 z-10 pl-5 pt-3 ">
-                    <div>
-                        <p className="text-xl">360</p>
-                    </div>
-                    <div>by Charli xcx</div>
+                <div className="w-3/4 h-20 z-10 pl-5 pt-3">
+                    <p className="text-sm whitespace-nowrap overflow-hidden text-ellipsis max-w-sm">
+                        <span className="inline-block animate-scroll">{post.postableSound?.sound?.title}</span>
+                    </p>
+                    <p className="text-sm mt-4">by {post.user?.name}</p>
                 </div>
-                <div
-                    className="w-1/4 h-20 z-10 pt-3 flex justify-center">
-                    <span>Pop</span>
+                <div className="w-1/4 h-20 z-10 pt-3 flex justify-center">
+                    <p className="text-sm">{post.postableSound?.sound?.genre?.name}</p>
                 </div>
             </div>
-            <div className="w-full pl-6 pb-1">
-                <span>---------o--------------------------------------</span>
+            <div className="w-full pl-6 pb-1 flex justify-center">
+                <audio controls className="w-full max-w-xs">
+                    <source src={post.postableSound?.sound?.audioPath} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                </audio>
             </div>
         </div>
-
-
     );
 }
