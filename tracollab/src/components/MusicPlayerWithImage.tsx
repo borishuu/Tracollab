d0b1e9347f1b87ce095ef237449033f8284aa74a
AlanSottile <alan.sottile@heig-vd.ch>
2024-08-30 14:03:21 +0200

    ending adding the display feature of the page

--- tracollab/src/components/MusicPlayerWithImage.tsx
+++ tracollab/src/components/MusicPlayerWithImage.tsx
@@ -1,17 +1,33 @@
 import MusicPlayer from "@/components/MusicPlayer";
 import React from 'react';
 
-export default function MusicPlayerWithImage() {
+export default function MusicPlayerWithImage({ post }) {
+    // Check if post and sound data exist
+    const hasSoundData = post?.sound;
+    const hasPicture = hasSoundData?.picture;
+
     return (
         <div className="flex-1 p-4 flex items-center">
-            <div className="w-24 flex items-center justify-center rounded-lg">
-                <div
-                    className="w-24 h-24 bg-[#8ACE00] flex items-center justify-center text-white rounded-full">
-                    <span className="text-sm bg-[#8ACE00]">Image</span>
-                </div>
+            <div className="w-32 flex items-center justify-center rounded-lg">
+                {hasSoundData && hasPicture ? (
+                    <div
+                        className="w-32 h-32 bg-[#8ACE00] flex items-center justify-center text-white rounded-full overflow-hidden text-center">
+                        <img
+                            src={post.sound.picture} // No need to re-check for sound.picture since it's already checked above
+                            alt={post.description || "No description"}
+                            width={96}
+                            height={96}
+                            className="object-cover w-full h-full"
+                        />
+                    </div>
+                ) : (
+                    <div className="w-24 h-24 bg-[#8ACE00] flex items-center justify-center text-white rounded-full">
+                        <span>🎵</span>
+                    </div>
+                )}
             </div>
             <div className="flex-1 pl-4">
-                <MusicPlayer/>
+                <MusicPlayer post={post} />
             </div>
         </div>
     );
