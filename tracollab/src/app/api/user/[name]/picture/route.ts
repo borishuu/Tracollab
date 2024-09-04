506512cde09523ef1907dd5bb67133fe9752c541
yahia1018 <yanis.ouadahi@gmail.com>
2024-09-04 16:45:22 +0200

    Removed double function declaration

--- tracollab/src/app/api/user/[name]/picture/route.ts
+++ tracollab/src/app/api/user/[name]/picture/route.ts
@@ -1,35 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import {SaveOptions, Storage} from '@google-cloud/storage';
 import { NextRequest, NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
 import { getUserData} from "@/app/lib/getUserData";
-import path from 'path';
-
-const serviceAccountKeyFile = path.join(process.cwd(), process.env.GC_KEY_PATH);
-const bucketName = 'tracollab-storage';
+import {uploadToGc} from "@/app/lib/gcUpload";
 
 const prisma = new PrismaClient();
-
-async function uploadToGc(file, folder) {
-    const timestamp = Date.now();
-    const fileExtension = path.extname(file.name);
-    const uniqueId = uuidv4();
-    const newFileName = `${timestamp}-${uniqueId}${fileExtension}`;
-
-    const buffer = await file.arrayBuffer();
-    const storage = new Storage({ keyFilename: serviceAccountKeyFile });
-
-    const filePath = `${folder}/${newFileName}`;
-
-    await storage.bucket(bucketName).file(filePath).save(Buffer.from(buffer), {
-        resumable: false,
-        contentType: file.type,
-    } as SaveOptions);
-
-    const publicUrl = `https://storage.googleapis.com/${bucketName}/${filePath}`;
-
-    return publicUrl;
-}
 
 export async function POST(req: NextRequest) {
     const form = await req.formData();
