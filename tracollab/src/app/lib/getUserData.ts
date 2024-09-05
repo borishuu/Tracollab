b21cd8c51719a87899692f86b53a0ce392157ff7
yahia1018 <yanis.ouadahi@gmail.com>
2024-09-05 08:05:20 +0000

    getUserData

--- tracollab/src/app/lib/getUserData.ts
+++ tracollab/src/app/lib/getUserData.ts
@@ -2,6 +2,7 @@ import {NextRequest} from "next/server";
 import {jwtVerify} from "jose";
 
 export async function getUserData(req: NextRequest) {
+
     const secret = new TextEncoder().encode(process.env.SECRET_KEY);
     const token = req.cookies.get('authToken')?.value;
 
